898e5ca5514d2106e24d231966b1017cb2315049
Erin Stalzer <erinstalzer@gmail.com>
2019-06-12 18:26:09 +0000

    add post /items code to item controller to handle submission of new item form

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -8,4 +8,18 @@ class ItemController < ApplicationController
     end
   end 
   
+  post '/items' do
+    if logged_in?
+      if params["name"] == ""
+        redirect '/items/new'
+      else
+        item = Item.create(name: params["name"], brand: params["brand"], variety: params["variety"], flavor: params["flavor"])
+        # @user = User.find_by(id: tweet.user_id)
+        # @tweets = @user.tweets
+        redirect '/pantry'
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
