f18e5561f35c7f82368c12ce10c0b69b8863a817
Erin Stalzer <erinstalzer@gmail.com>
2019-06-12 20:03:26 +0000

    set up show pantry view page to display items

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -33,4 +33,5 @@ class ItemController < ApplicationController
     end
   end
   
+  
 end
--- app/controllers/pantry_controller.rb
+++ app/controllers/pantry_controller.rb
@@ -1,6 +1,8 @@
 class PantryController < ApplicationController
   
   get '/pantry' do
+    @user = current_user
+    @pantry = @user.pantry
     erb :'pantry/show'
   end
   
