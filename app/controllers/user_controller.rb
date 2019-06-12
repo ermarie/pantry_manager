ee764c50088300194eb3caa6542fd5a363dfa8dc
Erin Stalzer <erinstalzer@gmail.com>
2019-06-12 16:45:19 +0000

    add post signup code to handle signup form submission

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,6 +7,16 @@ class UserController < ApplicationController
       erb :'users/signup'
     end
   end
+  
+  post "/signup" do
+    if params["username"] == "" || params["password"] == "" || params["email"] == ""
+      redirect '/signup'
+    else
+      user = User.create(username: params["username"], email: parmas["email"], password: params["password"])
+      session[:user_id] = user.id
+      #redirect '/pantry'
+    end
+  end
 
   
 end
