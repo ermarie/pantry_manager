4be7ed35380f9d88f8b664c64616c35321dadb77
Erin Stalzer <erinstalzer@gmail.com>
2019-06-13 00:13:34 +0000

    add and modify code to display edit item view page and path

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -45,7 +45,24 @@ class ItemController < ApplicationController
   get '/items/:id/edit' do
     if logged_in?
       @item = Item.find_by(id: params[:id])
+      @category = Category.find(@item.category_id)
       erb :'items/edit'
+    else
+      redirect '/login'
+    end
+  end
+  
+  patch '/items/:id' do
+    if logged_in?
+      if params["name"] == "" && params["brand"] == "" && params["variety"] == "" && params["flavor"] == "" && params["quantity"] == "" && params["quantity_type"] == "" && params["category_name"] == nil && params["category_id"] == nil
+        redirect "/items/#{params[:id]}/edit"
+      elsif params["category_name"] != nil && params["category_id"] != nil
+        redirect "/items/#{params[:id]}/edit"
+      else
+        @item = Item.find_by(id: params[:id])
+        @item.update(name: params["name"], brand: params["brand"], variety: params["variety"], flavor: params["flavor"], quantity: params["quantity"], quantity_type: params["quantity_type"], category_name: params["category_name"], category_id: params["category_id"] == nil)
+        erb :'items/show'
+      end
     else
       redirect '/login'
     end
