cc92cf0190212aae70c50dbffd0799cc4c27d4d3
Erin Stalzer <erinstalzer@gmail.com>
2019-08-26 15:04:25 -0400

    changing to postgres for heroku deploy

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,16 @@ ActiveRecord::Base.establish_connection(
 
 require './app/controllers/application_controller'
 require_all 'app'
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+    )
+end
