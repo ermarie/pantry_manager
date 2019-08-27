1c6e890dae2384e581dba45ca8deb464f718095d
Erin Stalzer <erinstalzer@gmail.com>
2019-08-27 11:27:05 -0400

    change enviro file to handle postgres on production and sqlite3 on dev

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,14 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+
 
 require './app/controllers/application_controller'
 require_all 'app'
+
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
 
 configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
